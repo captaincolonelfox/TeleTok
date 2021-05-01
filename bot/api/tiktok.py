590e95605b9d39a980b49933a7408154e30a4991
Igor Popov <lentrog@gmail.com>
2021-05-01 17:24:11 +0300

    Remove LikeeAPI

--- bot/api/tiktok.py
+++ bot/api/tiktok.py
@@ -1,5 +1,5 @@
 import json
-from typing import Any, Optional
+from typing import Optional, List
 
 from bot.api import API
 
@@ -7,15 +7,7 @@ from bot.api import API
 class TikTokAPI(API):
 
     @property
-    def headers(self) -> dict[str, Any]:
-        return {
-            "Referer": "https://www.tiktok.com/",
-            "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 "
-                          "(KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36",
-        }
-
-    @property
-    def links(self):
+    def links(self) -> List[str]:
         return ['tiktok.com']
 
     @property
--- bot/handlers/messages.py
+++ bot/handlers/messages.py
@@ -1,29 +1,24 @@
 import sentry_sdk
 from aiogram.types import Message
 from bot import dp, bot
-from bot.api import LikeeAPI, TikTokAPI
+from bot.api import TikTokAPI
 from bot.exception import HandleException
 from settings import DOWNLOAD_ERROR
 
 
-platforms = [TikTokAPI(), LikeeAPI()]
+platforms = [TikTokAPI()]
 
 
 @dp.message_handler()
 async def get_message(message: Message):
     for api in platforms:
         for video in await api.handle_message(message):
-            if video:
-                if video.content:
-                    await bot.send_video(
-                        message.chat.id, video.content, reply_to_message_id=message.message_id
-                    )
-                elif video.url:
-                    await bot.send_message(
-                        message.chat.id, video.url, reply_to_message_id=message.message_id
-                    )
+            if video and video.content:
+                return await bot.send_video(
+                    message.chat.id, video.content, reply_to_message_id=message.message_id
+                )
             else:
                 sentry_sdk.capture_exception(HandleException(message.text))
-                await bot.send_message(
+                return await bot.send_message(
                     message.chat.id, DOWNLOAD_ERROR, reply_to_message_id=message.message_id
                 )
