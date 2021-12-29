f235a4f4e9512c33191a111186a315e9516d9126
Igor Popov <lentrog@gmail.com>
2021-12-29 23:55:01 +0300

    Replace _parse_data() method with regexp groups

--- bot/api/base.py
+++ bot/api/base.py
@@ -1,15 +1,11 @@
 import asyncio
 import re
-import uuid
 from abc import ABC, abstractmethod
-from typing import Any, List, Optional
-
+from typing import Any, List
 import httpx
 import sentry_sdk
 from aiogram.types import Message
-from bs4 import BeautifulSoup
-from httpcore import TimeoutException
-from httpx import HTTPStatusError
+from httpx import HTTPStatusError, TimeoutException
 from bot.data import VideoData
 
 
@@ -53,23 +49,17 @@ class API(ABC):
 
     async def download_video(self, url: str, retries: int = 2) -> VideoData:
         for _ in range(retries):
-            async with httpx.AsyncClient(
-                    headers=self.headers, timeout=30, cookies=self.cookies, follow_redirects=True
-            ) as client:
-                try:
+            try:
+                async with httpx.AsyncClient(headers=self.headers, timeout=30,
+                                             cookies=self.cookies, follow_redirects=True) as client:
                     page = await client.get(url)
-                    soup = BeautifulSoup(page.content, 'html.parser')
-                    if data := soup(text=re.compile(self.regexp_key)):
-                        for script in data:
-                            if link := self._parse_data(script):
-                                if video := await client.get(link):
-                                    video.raise_for_status()
-                                    return VideoData(link, video.content)
-                except TimeoutException:
-                    pass
+                    for link in re.findall(self.regexp_key, page.text):
+                        link = link.encode('utf-8').decode('unicode_escape')
+                        if video := await client.get(link):
+                            video.raise_for_status()
+                            return VideoData(link, video.content)
+            except TimeoutException:
+                pass
             await asyncio.sleep(0.5)
         return VideoData()
 
-    @abstractmethod
-    def _parse_data(self, script: str) -> Optional[str]:
-        pass
--- bot/api/tiktok.py
+++ bot/api/tiktok.py
@@ -1,7 +1,4 @@
-import json
-import cytoolz
-from typing import Optional, List
-
+from typing import List
 from bot.api import API
 
 
@@ -13,13 +10,7 @@ class TikTokAPI(API):
 
     @property
     def regexp_key(self) -> str:
-        return r'"downloadAddr":"'
-
-    def _parse_data(self, script: str) -> Optional[str]:
-        return cytoolz.get_in(
-            ['props', 'pageProps', 'itemInfo', 'itemStruct', 'video', 'downloadAddr'],
-            json.loads(script)
-        )
+        return r'downloadAddr":"(.*?)",'
 
 
 class MobileTikTokAPI(API):
@@ -30,14 +21,4 @@ class MobileTikTokAPI(API):
 
     @property
     def regexp_key(self) -> str:
-        return r'"video":{"urls"'
-
-    def _parse_data(self, script: str) -> Optional[str]:
-        script = script.split(' = ', maxsplit=1)[-1]
-        urls = cytoolz.get_in(
-            ['/v/:id', 'videoData', 'itemInfos', 'video', 'urls'],
-            json.loads(script),
-            default=[]
-        )
-        for url in urls:
-            return url
+        return r'urls":\["(.*?)"\]'
