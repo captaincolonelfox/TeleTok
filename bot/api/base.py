b39cfb545bca57ca8562e661c97db8c0a99bc05b
Igor Popov <lentrog@gmail.com>
2021-12-30 00:20:10 +0300

    Refactor methods to use tuples and bytes instead of VideoData

--- bot/api/base.py
+++ bot/api/base.py
@@ -4,10 +4,7 @@ import re
 from abc import ABC, abstractmethod
 from typing import Any, List
 import httpx
-import sentry_sdk
 from aiogram.types import Message
-from httpx import HTTPStatusError
-from bot.data import VideoData
 
 
 def retries(times: int):
@@ -49,20 +46,16 @@ class API(ABC):
     def regexp_key(self) -> str:
         return 'key'
 
-    async def handle_message(self, message: Message) -> List[VideoData]:
+    async def handle_message(self, message: Message) -> tuple[bytes, ...]:
         urls = []
         for e in message.entities:
             for link in self.links:
                 if link in (url := message.text[e.offset:e.offset + e.length]):
                     urls.append(url if url.startswith('http') else f'https://{url}')
-        try:
-            return [await self.download_video(url) for url in urls]
-        except (KeyError, HTTPStatusError) as ex:
-            sentry_sdk.capture_exception(ex)
-        return []
+        return tuple(filter(None, [await self.download_video(url) for url in urls]))
 
     @retries(times=2)
-    async def download_video(self, url: str) -> VideoData:
+    async def download_video(self, url: str) -> bytes:
         async with httpx.AsyncClient(headers=self.headers, timeout=30,
                                      cookies=self.cookies, follow_redirects=True) as client:
             page = await client.get(url)
@@ -70,5 +63,4 @@ class API(ABC):
                 link = link.encode('utf-8').decode('unicode_escape')
                 if video := await client.get(link):
                     video.raise_for_status()
-                    return VideoData(link, video.content)
-        return VideoData()
+                    return video.content
--- bot/handlers/messages.py
+++ bot/handlers/messages.py
@@ -9,9 +9,7 @@ platforms = [MobileTikTokAPI(), TikTokAPI()]
 @dp.message_handler()
 async def get_message(message: Message):
     for api in platforms:
-        if videos := [v for v in await api.handle_message(message) if v and v.content]:
+        if videos := await api.handle_message(message):
             for video in videos:
-                await bot.send_video(
-                    message.chat.id, video.content, reply_to_message_id=message.message_id
-                )
+                await bot.send_video(message.chat.id, video, reply_to_message_id=message.message_id)
             break
