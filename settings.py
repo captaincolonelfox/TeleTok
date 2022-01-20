a1a0518be7cba43eae137f018845869ca6b54a96
Igor <lentrog@gmail.com>
2022-01-20 18:11:23 +0300

    Remove default value for USER_AGENT env variable

--- settings.py
+++ settings.py
@@ -3,4 +3,4 @@ import os
 API_TOKEN = os.environ.get('API_TOKEN')
 SENTRY_DSN = os.getenv('SENTRY_DSN')
 ENVIRONMENT = os.getenv('ENVIRONMENT', 'Local')
-USER_AGENT = os.getenv('USER_AGENT', 'Mozilla/5.0 (X11; Linux x86_64; rv:96.0) Gecko/20100101 Firefox/96.0')
+USER_AGENT = os.getenv('USER_AGENT')
